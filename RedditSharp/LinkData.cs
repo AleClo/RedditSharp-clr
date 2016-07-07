1c069802f364e5095e2b7b56da72a06719a04f26
Andrew <pimanac@users.noreply.github.com>
2016-07-07 01:22:32 -0400

    move these into individual classes for simplicity

--- RedditSharp/LinkData.cs
+++ RedditSharp/LinkData.cs
@@ -1,6 +1,6 @@
 ﻿namespace RedditSharp
 {
-   internal class LinkData : SubmitData
+   internal class LinkData
    {
       [RedditAPIName("extension")]
       internal string Extension { get; set; }
@@ -8,10 +8,38 @@
       [RedditAPIName("url")]
       internal string URL { get; set; }
 
+      [RedditAPIName("api_type")]
+      internal string APIType { get; set; }
+
+      [RedditAPIName("kind")]
+      internal string Kind { get; set; }
+
+      [RedditAPIName("sr")]
+      internal string Subreddit { get; set; }
+
+      [RedditAPIName("uh")]
+      internal string UserHash { get; set; }
+
+      [RedditAPIName("title")]
+      internal string Title { get; set; }
+
+      [RedditAPIName("iden")]
+      internal string Iden { get; set; }
+
+      [RedditAPIName("captcha")]
+      internal string Captcha { get; set; }
+
+      [RedditAPIName("resubmit")]
+      internal bool Resubmit { get; set; }
+
       internal LinkData()
       {
+         APIType = "json";
          Extension = "json";
          Kind = "link";
       }
+
+      
+
    }
 }
--- RedditSharp/TextData.cs
+++ RedditSharp/TextData.cs
@@ -1,13 +1,38 @@
 ﻿namespace RedditSharp
 {
-   internal class TextData : SubmitData
+   internal class TextData
    {
       [RedditAPIName("text")]
       internal string Text { get; set; }
 
+      [RedditAPIName("api_type")]
+      internal string APIType { get; set; }
+
+      [RedditAPIName("kind")]
+      internal string Kind { get; set; }
+
+      [RedditAPIName("sr")]
+      internal string Subreddit { get; set; }
+
+      [RedditAPIName("uh")]
+      internal string UserHash { get; set; }
+
+      [RedditAPIName("title")]
+      internal string Title { get; set; }
+
+      [RedditAPIName("iden")]
+      internal string Iden { get; set; }
+
+      [RedditAPIName("captcha")]
+      internal string Captcha { get; set; }
+
+      [RedditAPIName("resubmit")]
+      internal bool Resubmit { get; set; }
+
       internal TextData()
       {
          Kind = "self";
+         APIType = "json";
       }
    }
 }
